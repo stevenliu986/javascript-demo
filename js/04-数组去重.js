906113c3c971b7a636d9006968e0664945bb54c7
StevenLiu <stevenliu986@gmail.com>
2023-07-31 11:41:19 +1000

    update 2nd solution

--- js/04-数组去重.js
+++ js/04-数组去重.js
@@ -2,7 +2,7 @@
   方案一：循环原有数组中的每一项，每拿到一项就往新数组中添加，
   添加之前验证新数组中是否存在这一项，不存在再增加 
 */
-let arr = [1, 2, 3, 1, 2, 1, 2, 2, 1, 1, 2, 3, 2, 1];
+let arr = [1, 2, 3, 1, 2, 1, 2, 3, 2, 1, 2, 3];
 
 let newArr = [];
 arr.forEach((item) => {
@@ -13,3 +13,16 @@ arr.forEach((item) => {
 });
 
 console.log(newArr);
+
+// 方案二：使用splice方法去重(不可行，有数组塌陷问题，需要在去重的时候，人为将索引更改来避免数组塌陷问题)
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] === arr[j]) {
+      arr.splice(j, 1);
+      j--;
+    }
+  }
+  return arr;
+}
+
+console.log(arr);
