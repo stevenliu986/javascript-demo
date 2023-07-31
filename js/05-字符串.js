eb374f5d65354bd08db046ec0bb135a351a9567c
StevenLiu <stevenliu986@gmail.com>
2023-07-31 15:38:00 +1000

    update

--- js/05-字符串.js
+++ js/05-字符串.js
@@ -6,3 +6,12 @@ console.log(str.indexOf("@")); // 返回-1，说明@不存在
 
 // 验证Script作为一个整体第一次出现的位置，返回的索引是第一个字符所在位置的索引
 console.log(str.indexOf("Script"));
+
+let strArr = str.split("|");
+console.log(strArr);
+// str = str.replace(",", "|");
+// console.log(str);
+
+// 使用正则表达式来实现全部替换
+str = str.replace(/,/g, "|");
+console.log(str);
