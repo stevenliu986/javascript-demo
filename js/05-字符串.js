495562d8328d485c5731a264f93d50309816674c
StevenLiu <stevenliu986@gmail.com>
2023-07-31 18:36:42 +1000

    add new solution

--- js/05-字符串.js
+++ js/05-字符串.js
@@ -21,7 +21,7 @@ console.log(str);
 let time = "2019-7-24 12:6:23";
 
 // 方案一
-time =
+/* time =
   time
     .replace("-", "年")
     .replace("-", "月")
@@ -29,6 +29,31 @@ time =
     .replace(":", "时")
     .replace(":", "分") + "秒";
 
-console.log(time);
+console.log(time); */
 
 // 方案二：获取到年月日小时分钟秒这几个值后，想拼成什么效果就拼成什么效果
+// 使用正则表达式来截取
+let timeArr = time.split(/(?: |-|:)/g);
+
+let addZero = (val) => (val = val.length < 2 ? "0" + val : val);
+
+time =
+  timeArr[0] + "年" + addZero(timeArr[1]) + "月" + addZero(timeArr[2]) + "日";
+console.log(time);
+
+// 还是繁琐
+/* let l = time.split(" ");
+let m = l[0].split("-");
+let n = l[1].split(":");
+console.log(m, n); */
+
+// 麻烦, 获取值的方法，基于indexOf获取指定符号的索引，基于substring一点点截取
+/* let n = time.indexOf("-");
+let m = time.lastIndexOf("-");
+let x = time.indexOf(" ");
+let y = time.indexOf(":");
+let z = time.lastIndexOf(":");
+let year = time.substring(0, n);
+let month = time.substring(n + 1, m);
+let date = time.substring(m + 1, x);
+console.log(year, month, date); */
