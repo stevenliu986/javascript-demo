31c4deec224d3ce0296803e7b29a605094e47a87
StevenLiu <stevenliu986@gmail.com>
2023-08-02 16:43:52 +1000

    add exercises

--- js/06-变量提升及闭包作用域.js
+++ js/06-变量提升及闭包作用域.js
@@ -5,8 +5,8 @@ b = 13;
 console.log(a);
 console.log(c); // 会报错
 
-// 闭包作用域练习题
-var i = 5;
+// 闭包作用域练习题1
+/* var i = 5;
 function fn(i) {
   return function (n) {
     console.log(n + ++i);
@@ -17,4 +17,20 @@ f(2);
 fn(3)(4);
 fn(5)(6);
 f(7);
+console.log(i); */
+
+// 闭包作用域练习题2
+var i = 20;
+function fn() {
+  i -= 2;
+  return function (n) {
+    console.log(++i - n);
+  };
+}
+var f = fn();
+f(1);
+f(2);
+fn()(3);
+fn()(4);
+f(5);
 console.log(i);
