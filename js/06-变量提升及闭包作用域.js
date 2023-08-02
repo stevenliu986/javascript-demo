de8ab8f734c765c53b06cde32f1bbc3a11ff3d69
StevenLiu <stevenliu986@gmail.com>
2023-08-02 14:57:59 +1000

    add new lines

--- js/06-变量提升及闭包作用域.js
+++ js/06-变量提升及闭包作用域.js
@@ -4,3 +4,17 @@ var b = a;
 b = 13;
 console.log(a);
 console.log(c); // 会报错
+
+// 闭包作用域练习题
+var i = 5;
+function fn(i) {
+  return function (n) {
+    console.log(n + ++i);
+  };
+}
+var f = fn(1);
+f(2);
+fn(3)(4);
+fn(5)(6);
+f(7);
+console.log(i);
