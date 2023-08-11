372f3d4511ae9049bb1caf0df20b4aa64491d589
StevenLiu <stevenliu986@gmail.com>
2023-08-11 14:44:23 +1000

    add new lines

--- js/08-原型练习题.js
+++ js/08-原型练习题.js
@@ -72,4 +72,4 @@ function C3(name) {
 C1.prototype.name = "Tom";
 C2.prototype.name = "Tom";
 C3.prototype.name = "Tom";
-alert(new C1().name + new C2().name + new C3().name);
+alert(new C1().name + new C2().name + new C3().name); // "Tomundefinedjoin"
