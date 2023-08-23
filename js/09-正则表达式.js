8b358223a98e02da3dfd985677b0c56a9cb269d5
StevenLiu <stevenliu986@gmail.com>
2023-08-23 11:25:08 +1000

    add new lines

--- js/09-正则表达式.js
+++ js/09-正则表达式.js
@@ -14,3 +14,9 @@ let reg4 = /^2.3$/;
 console.log(reg4.test("2.3")); // true
 console.log(reg4.test("2@3")); // true
 console.log(reg4.test("23")); // false
+
+// 如果需匹配2.3，就需要使用\转义字符
+let reg5 = /^2\.3$/;
+console.log(reg4.test("2.3")); // true
+console.log(reg4.test("2@3")); // false
+console.log(reg4.test("23")); // false
