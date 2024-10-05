c8d0ebeadcf4449a3d4efe26cd10bf27f33cac58
StevenLiu <stevenliu986@gmail.com>
2024-10-06 00:06:47 +1000

    change variable

--- js/11-closure.js
+++ js/11-closure.js
@@ -8,7 +8,7 @@ wait("Hello");
 
 // 闭包就是能够读取其他函数内部变量的函数
 function foo01() {
-  var privateCounter = 0;
+  let privateCounter = 0;
   function counter(num) {
     return (privateCounter += num);
   }
